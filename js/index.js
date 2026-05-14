fe1baf874bd2fef488e6963944ac0c966db4dbad
Pedro Oliveira <pedrogamer212004@gmail.com>
2026-05-13 23:46:57 -0300

    fix: Formulário

--- js/index.js
+++ js/index.js
@@ -53,8 +53,13 @@ phoneInput.addEventListener('input', (e) => {
 });
 
 
-function formatDate(date) {
-    const [year, month, day] = date.split('-');
+function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate() + 1).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
     return `${day}/${month}/${year}`;
 }
 
